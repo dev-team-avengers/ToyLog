0fcd7ea846baa1a52f3689e048fa6adccd5957fc
YoungWukJeon <kdh10080@gmail.com>
2020-05-31 18:00:45 +0900

    initial commit

---
+++ src/main/java/team/avengers/toylog/ToylogApplication.java
@@ -0,0 +1,13 @@
+package team.avengers.toylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToylogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/avengers/toylog/ToylogApplicationTests.java
@@ -0,0 +1,13 @@
+package team.avengers.toylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
