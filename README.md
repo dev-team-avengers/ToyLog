5bc79ea278c411ac33fc3ca9bc2ee52d24415e9f
YoungWukJeon <kdh10080@gmail.com>
2020-06-02 06:02:37 +0900

    배포 파일 수정(deploy.sh 실행했을 때 올바르게 안되는거 같음)

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # ToyLog
 
+## Spec
+- Spring Boot 2.3.0
+- OpenJDK11
+
 ## Links
 - Issue Tracker : https://trello.com/b/iZCFQJFg/toylog
 - Github : https://github.com/dev-team-avengers/ToyLog
 
+## Deployment
+```shell script
+deploy/{profile}/deploy.sh {profile}
+```
+
+## About Development
+- OpenAPI(using docker) : http://localhost:9000/swagger-ui.html
+- OpenAPI(using ide) : http://localhost:8080/swagger-ui.html
+---
+### Intellij Community Edition을 사용할 때 IDE에서 실행하는 법
+1. Run - Edit Configurations 연다
+2. Add New Configurations로 Application을 추가한다.
+3. Main class를 team.avengers.toylog.ToyLogApplication로 지정
+4. Program arguments에 --spring.profiles.active=dev 추가 (실행하려는 profile에 맞게 변경 가능)
+5. Use classpath of module을 toylog-api.main으로 설정
+6. JRE 사용할 JRE 지정(11 버전)
--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -7,5 +7,7 @@ echo "SPRING_PROFILE - ${SPRING_PROFILE}"
 ## DOCKER LOGIN
 # docker image build -t team-avengers/toylog-api:latest deploy/${SPRING_PROFILE}/Dockerfile -e SPRING_PROFILE=${SPRING_PROFILE}
 # docker image build -t team-avengers/toylog-api:latest . --build-arg SPRING_PROFILE=dev
-docker image build -t team-avengers/toylog-api:latest .
-docker container run -t -p 9000:8080 team-avengers/toylog-api:latest
+docker build --no-cache -t team-avengers/toylog-api:latest .
+docker run --rm -t -p 9000:8080 --name toylog-api team-avengers/toylog-api:latest
+# docker run --rm -t -p 9000:8080 -e "TZ=Asia/Seoul" --name toylog-api team-avengers/toylog-api:latest <- windows
+# docker run --rm -t -p 9000:8080 -v /etc/localtime:/etc/localtime:ro --name toylog-api team-avengers/toylog-api:latest <- Linux or MacOS
