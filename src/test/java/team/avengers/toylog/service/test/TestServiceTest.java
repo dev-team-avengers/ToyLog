ea0a35e96d18cac491a2f8e0a1e4a2d01b90b647
YoungWukJeon <kdh10080@gmail.com>
2020-06-02 22:40:07 +0900

    mockito를 사용해 TestService MockTest 예제 만들기(build.gradle mockito 버전업. 버전 이슈가 있었음. 내용은 잘 기억이 안남)

--- src/test/java/team/avengers/toylog/service/test/TestServiceTest.java
+++ src/test/java/team/avengers/toylog/service/test/TestServiceTest.java
@@ -1,31 +1,55 @@
 package team.avengers.toylog.service.test;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import team.avengers.toylog.advice.exception.TestNotFoundException;
+import team.avengers.toylog.model.ApiResponseDto;
+import team.avengers.toylog.model.test.TestResponseDto;
 import team.avengers.toylog.persistence.h2.entity.TestEntity;
-import team.avengers.toylog.service.ResponseService;
+import team.avengers.toylog.persistence.h2.repository.TestRepository;
 
-import javax.transaction.Transactional;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.BDDAssertions.*;
 
 import static org.mockito.BDDMockito.*;
 
-@ActiveProfiles(value = "dev")
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class TestServiceTest {
     @InjectMocks
     private TestService testService;
-    @InjectMocks
-    private ResponseService responseService;
+
+    @Mock
+    private TestRepository testRepository;
+
+    @Test
+    void 존재하는_Id를_조회했을_경우() {
+        // given -> mocking
+        final Long id = anyLong();
+        given(testRepository.findById(id))
+                .willReturn(Optional.of(TestEntity.builder().id(id).build()));
+
+        // when -> 실제 테스트할 내용
+        ApiResponseDto<TestResponseDto> dto = testService.getTestById(id);
+
+        // then -> 검증
+        assertEquals(id, dto.getData().getId());
+        verify(testRepository, times(1))
+                .findById(id);
+    }
 
     @Test
     void Id가_존재하지_않을_경우() {
-//        given(testService.getTestById(1L))
-//                .willReturn(responseService.getFailureApiResponseDto(-9998, "", null));
+        final Long id = anyLong();
+        given(testRepository.findById(id))
+                .willReturn(Optional.empty());
+
+        assertThrows(TestNotFoundException.class, () -> {
+            ApiResponseDto<TestResponseDto> dto = testService.getTestById(id);
+        });
     }
 }
