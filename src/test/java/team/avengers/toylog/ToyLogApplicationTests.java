6902bb10e6067c9556b11a601b7cf311f50ae172
YoungWukJeon <kdh10080@gmail.com>
2020-07-01 01:47:17 +0900

    ToyLogApplicationTests에 Profile 설정 추가(이거 때문에 Github Actions에서 에러남)

--- src/test/java/team/avengers/toylog/ToyLogApplicationTests.java
+++ src/test/java/team/avengers/toylog/ToyLogApplicationTests.java
@@ -2,8 +2,10 @@ package team.avengers.toylog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "dev")
 class ToyLogApplicationTests {
 
 	@Test
