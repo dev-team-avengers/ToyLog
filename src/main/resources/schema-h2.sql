0b1725bd5498bcf3f1591e241c3d79145eba97a4
Young-wuk Jeon <dev.kani@kakao.com>
2020-07-06 21:36:52 +0900

    Update schema-h2.sql

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -3,47 +3,45 @@ drop sequence if exists hibernate_sequence;
 create sequence hibernate_sequence start with 1 increment by 1;
 
 drop table test if exists;
-drop table user_tbl if exists;
-drop table board_tbl if exists;
-drop table comment_tbl if exists;
-drop table pin_tbl if exists;
-
 create table test(
     id bigint primary key auto_increment,
     name varchar(20) not null
 );
 
+drop table user_tbl if exists;
 create table user_tbl(
     idx INT PRIMARY KEY AUTO_INCREMENT,
     id VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    auth VARCHAR(255),
+    profile_image VARCHAR(255),
+    third_party_type ENUM('kakao'),
+    third_party_id VARCHAR(255),
     regist_date DATETIME DEFAULT NOW(),
     update_date DATETIME DEFAULT NOW(),
     last_login_date DATETIME DEFAULT NOW()
 );
-CREATE INDEX ON user_tbl(id);
 
+drop table board_tbl if exists;
 create table board_tbl(
     idx INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(100),
-    content TEXT ,
+    user_idx INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    tag VARCHAR(255),
     views INT DEFAULT 0,
     pin boolean DEFAULT false,
-    regist_idx INT,
     regist_date DATETIME DEFAULT NOW(),
     update_date DATETIME DEFAULT NOW()
 );
-CREATE INDEX ON board_tbl(regist_idx);
 
+drop table comment_tbl if exists;
 create table comment_tbl(
     idx INT PRIMARY KEY AUTO_INCREMENT,
-    board_idx INT,
-    parent_idx INT NULL,
-    content VARCHAR(255),
+    board_idx INT NOT NULL,
+    parent_idx INT,
+    user_idx INT NOT NULL,
     order_no SMALLINT,
-    regist_idx INT,
+    content VARCHAR(255) NOT NULL,
     regist_date DATETIME DEFAULT NOW(),
     update_date DATETIME DEFAULT NOW()
 );
-CREATE INDEX ON comment_tbl(parent_idx);
